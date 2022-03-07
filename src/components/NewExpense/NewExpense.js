050272a1ff980a4cc15c01d36381208ca26a19e7
shvnmahajan <shravanmahajan123@gmail.com>
2022-03-08 00:22:39 +0530

    Added button to show hidden form and hide after form submit

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-
-  
+  const [state, setState] = useState("hidden-form");
 
   const submitExpenseDataHandler = (enteredData) => {
     const expenseData = {
       ...enteredData,
-      id : Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
-    //console.log(expenseData);
+    setState("hidden-form");
+  };
+
+  const addNewExpenseClickHandler = () => {
+    setState("visible-form");
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSubmitExpenseData={submitExpenseDataHandler} />
+      {state === "visible-form" ? (
+        <ExpenseForm onSubmitExpenseData={submitExpenseDataHandler} />
+      ) : (
+        <button onClick={addNewExpenseClickHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
